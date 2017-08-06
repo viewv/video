916d3c4299f3ff40096f0a79e9958b65a520e8b9
Six Edge <sixedge111@gmail.com>
2017-08-06 13:57:14 +0800

    ...

--- src/main/java/com/c/v/action/HomeController.java
+++ src/main/java/com/c/v/action/HomeController.java
@@ -23,13 +23,13 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(final Locale locale, final Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		final Date date = new Date();
+		final DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
-		String formattedDate = dateFormat.format(date);
+		final String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
 		
